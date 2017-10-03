a30a586734eca421a39745843aa2c20e76f360b7
Raul Carlos De Almeida Junior <rcjunior2@stefanini.com>
2017-10-03 13:34:19 -0300

    criado um junit para o modulo transaction

---
+++ transaction-module/src/main/java/br/com/raulcaj/transactionmodule/domain/AccountServiceRestClient.java
@@ -0,0 +1,93 @@
+package br.com.raulcaj.transactionmodule.domain;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.raulcaj.transactionmodule.controller.NotAcceptableException;
+
+@Profile("production")
+@Service
+public class AccountServiceRestClient implements AccountService {
+
+	@Value("${service.account.path.getById}")
+	private String getByIdPath;
+
+	@Value("${service.account.path.patchLimit}")
+	private String patchLimitPath;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Value("${service.account.name}")
+	private String accountServiceName;
+
+	private RestTemplate restTemplate = createRestTemplate();
+
+	private RestTemplate createRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		// 4 debug purposes
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(50000);
+		requestFactory.setReadTimeout(50000);
+		restTemplate.setRequestFactory(requestFactory);
+		return restTemplate;
+	}
+
+	private String getUri(final String operationPath) {
+		final Optional<ServiceInstance> service = discoveryClient.getInstances(accountServiceName).stream().findFirst();
+		return service.map(ServiceInstance::getUri).map(Object::toString).orElse("http://localhost:8080")
+				.concat(operationPath);
+	}
+	
+	/* (non-Javadoc)
+	 * @see br.com.raulcaj.transactionmodule.domain.AccountService#executeAccountLimitUpdate(java.lang.Long, org.springframework.data.util.Pair)
+	 */
+	@Override
+	public void executeAccountLimitUpdate(final Long accountId, final Pair<BigDecimal, BigDecimal> amountSpent) throws NotAcceptableException {
+		final AccountRequest accountInfo = AccountRequest.createAccountRequest(accountId, amountSpent);
+		try {
+			final ResponseEntity<Void> response = restTemplate.exchange(getUri(patchLimitPath), HttpMethod.PATCH,
+					new HttpEntity<>(accountInfo), Void.class, accountId);
+			if (!HttpStatus.OK.equals(response.getStatusCode())) {
+				throw new NotAcceptableException("Could not update account limit");
+			}
+		} catch (HttpClientErrorException e) {
+			if (HttpStatus.NOT_ACCEPTABLE.equals(e.getStatusCode())) {
+				throw new NotAcceptableException("Limit exceeded");
+			}
+			throw e;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see br.com.raulcaj.transactionmodule.domain.AccountService#accountNotExist(java.lang.Long)
+	 */
+	@Override
+	public boolean accountNotExist(Long accountId) throws NotAcceptableException {
+		final ResponseEntity<AccountRequest> response = restTemplate.exchange(getUri(getByIdPath), HttpMethod.GET, null,
+				AccountRequest.class, accountId);
+		if (HttpStatus.NOT_FOUND.equals(response.getStatusCode())) {
+			return true;
+		}
+		if (HttpStatus.OK.equals(response.getStatusCode())) {
+			return !accountId.equals(response.getBody().getId());
+		}
+		throw new NotAcceptableException("Error trying to look for this account");
+	}
+
+}
--- transaction-module/src/test/java/br/com/raulcaj/transactionmodule/TransactionModuleApplicationTests.java
+++ transaction-module/src/test/java/br/com/raulcaj/transactionmodule/TransactionModuleApplicationTests.java
@@ -1,16 +1,86 @@
 package br.com.raulcaj.transactionmodule;
 
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.raulcaj.transactionmodule.TransactionModuleApplicationTests.TestConfig;
+import br.com.raulcaj.transactionmodule.controller.NotAcceptableException;
+import br.com.raulcaj.transactionmodule.domain.AccountService;
+import br.com.raulcaj.transactionmodule.domain.TransactionRequest;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes=TestConfig.class)
+@ActiveProfiles("test")
 public class TransactionModuleApplicationTests {
 
+	@Configuration
+	@Import(TransactionModuleApplication.class)
+	@Profile("test")
+	public static class TestConfig {
+		private static final Map<Long, Pair<BigDecimal, BigDecimal>> REPOSITORY = new HashMap<>();
+		static {
+			REPOSITORY.put(1L, Pair.of(new BigDecimal(5000L), new BigDecimal(5000L)));
+		}
+		@Bean
+		public AccountService accountService() {
+			return new AccountService() {
+				@Override
+				public void executeAccountLimitUpdate(Long accountId, Pair<BigDecimal, BigDecimal> amount)
+						throws NotAcceptableException {
+					if (!REPOSITORY.containsKey(accountId)) {
+						throw new NotAcceptableException();
+					}
+					final Pair<BigDecimal, BigDecimal> account = REPOSITORY.get(accountId);
+					REPOSITORY.put(accountId, Pair.of(account.getFirst().add(amount.getFirst()),
+							account.getSecond().add(amount.getSecond())));
+				}
+
+				@Override
+				public boolean accountNotExist(Long accountId) throws NotAcceptableException {
+					return !REPOSITORY.containsKey(accountId);
+				}
+			};
+		}
+	}
+
+	@LocalServerPort
+	private long localPort;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	private String getUri(String path) {
+		return String.format("http://localhost:%d%s", localPort, path);
+	}
+
 	@Test
-	public void contextLoads() {
+	public void createCreditTx() {
+		final TransactionRequest tr = TransactionRequest.createTxRequest(1L, 1L, new BigDecimal(100L));
+		final ResponseEntity<Void> response = restTemplate.exchange(getUri("/v1/transactions"), HttpMethod.POST,
+				new HttpEntity<>(tr), Void.class);
+		assertTrue(HttpStatus.OK.equals(response.getStatusCode()));
 	}
 
 }
